4170ca5037e88e2587cf35ba13e607f2d78a0f71
Niklas Munkes <niklas.munkes@student.uni-tuebingen.de>
2025-05-19 17:40:21 +0200

    added persist. cit data storage

--- src/main.py
+++ src/main.py
@@ -55,91 +55,122 @@ def get_next_block_lines(current_index, bib_list, block_label, current_lines=[])
     return current_lines
 
 
-def get_citation_data_from_bbl(path, additional_citation_ids=[]):
-    bib_list = []
-    with open(path, "r") as bib:
-        for line in bib.readlines():
-            bib_list.append(line.rstrip())
-    
-    citations_data = {}
-    ids = []
-    for i, line in enumerate(bib_list):
-        # identify new entry
-        if line.startswith("\\bibitem"):
+def get_citations_data_from_bbl(path, additional_citation_records=None, force_download=False):
+    source_id = path.split("/")[1]
+    data_path = f"data/citations_data/cd_source-{source_id}.json"
 
-            # identify bib id
-            bibitem_lines = get_next_block_lines(i-1, bib_list, "\\bibitem")
-            bib_id = ""
-            for l in bibitem_lines:
-                bib_id += l
-            bib_id = bib_id.split("]{")[-1].split("}")[0]
+    # determine whether to download or load from disk
+    file_found = None
+    try:
+        with open(data_path) as f:
+            pass
+        file_found = True
+    except FileNotFoundError:
+        file_found = False
 
-            # identify title
-            title_lines = get_next_block_lines(i, bib_list, "\\newblock")
-            title = ""
-            for title_idx in range(len(title_lines)):
-                if title_idx == 0:
-                    title += title_lines[title_idx].lstrip("\\newblock ")
-                else:
-                    title += title_lines[title_idx].lstrip(" ")
-            title = title.rstrip(".")
+    if not force_download and file_found:
+        with open(data_path, "r") as f:
+            citations_data = json.load(f)
+        ids = [x[1]["arxiv_id"] for x in citations_data.items()]
+    else: # download
+        bib_list = []
+        with open(path, "r") as bib:
+            for line in bib.readlines():
+                bib_list.append(line.rstrip())
+        
+        citations_data = {}
+        ids = []
+        for i, line in enumerate(bib_list):
+            # identify new entry
+            if line.startswith("\\bibitem"):
 
-            # use title to search for and retrieve the arxiv id
-            title_for_url = title.replace(" ", "+")
-            url = f'https://export.arxiv.org/api/query?search_query={title_for_url}&searchtype=title&start=0&max_results={ARXIV_MAX_RESULTS}'
-            data = urllib.request.urlopen(url)
-            xml = data.read().decode('utf-8')
-            try: # skip to the actual search results
-                xml = xml.split("<entry>")[1:]
-            except IndexError:
-                continue # skip this bib item if the search does not yield a result
-            xml = xml.replace(">\n", ">SPLIT").split("SPLIT")
-            xml = [# cleanup whitespace and linebraks
-                item.lstrip().replace("\n", " ")
-                for item in xml
-            ]
+                # identify bib id
+                bibitem_lines = get_next_block_lines(i-1, bib_list, "\\bibitem")
+                bib_id = ""
+                for l in bibitem_lines:
+                    bib_id += l
+                bib_id = bib_id.split("]{")[-1].split("}")[0]
 
-            # filter relevant elements and map them
-            xml_dict = {"bib_id": bib_id}
-            for search_result_entry in xml:
-                # check if the paper has the correct title
-                
-                if not : 
+                # identify title
+                title_lines = get_next_block_lines(i, bib_list, "\\newblock")
+                title = ""
+                for title_idx in range(len(title_lines)):
+                    if title_idx == 0:
+                        title += title_lines[title_idx].lstrip("\\newblock ")
+                    else:
+                        title += title_lines[title_idx].lstrip(" ")
+                title = title.rstrip(".")
 
-                for label in ["<id>", "<title>", "<summary>", "<name>"]:
-                    if search_result_entry.startswith(label):
-                        clean_label = label.lstrip("<").rstrip(">")
-                        item_body = search_result_entry.lstrip(label).rstrip(label.replace("<", "</"))
-                        if clean_label == "name":
-                            try: # check if entry already exists (for multiple author names)
-                                xml_dict[clean_label] += f" {item_body}"
-                            except KeyError:
-                                xml_dict[clean_label] = item_body
-                        elif clean_label == "id":
-                            xml_dict["arxiv_id"] = item_body.split("/")[-1]
-                        else:
-                            xml_dict[clean_label] = item_body
+                # use title to search for and retrieve the arxiv id
+                title_for_url = title.replace(" ", "+")
+                url = f'https://export.arxiv.org/api/query?search_query={title_for_url}&searchtype=title&start=0&max_results={ARXIV_MAX_RESULTS}'
+                data = urllib.request.urlopen(url)
+                xml = data.read().decode('utf-8')
+                try: # skip to the actual search results
+                    xml = xml.split("<entry>")[1:]
+                except IndexError:
+                    continue # skip this bib item if the search does not yield a result
 
-            # compare titles
-            t1 = title.lower().replace(" ", "")
-            t2 = xml_dict["title"].lower().replace(" ", "")
-            if not t1 == t2:
-                # print(f"{t1} != {t2} ?!")
-                # do not add this citation entry to citation_data bc the arxiv id could not be determined
-                # TODO: maybe get the latex paper from somewhere else?
-                continue 
-            else:
-                # print("titles match, yay!")
-                pass
+                # extract and store information from search result entries
+                xml_dict = {"bib_id": f"{bib_id}"}
+                for search_result_entry in xml:
+                    entry = [ # cleanup whitespace and linebraks
+                        item.lstrip().replace("\n", " ")
+                        for item in search_result_entry.replace(">\n", ">SPLIT").split("SPLIT")
+                    ]
 
-            # store this citation (xml_dict) in citation_data
-            citations_data[bib_id] = xml_dict
+                    # get the title of this entry, if possible
+                    entry_title = ""
+                    for entry_item in entry:
+                        if entry_item.startswith("<title>"):
+                            entry_title = entry_item.lstrip("<title>").rstrip("</title>")
+                            break
 
-            # add id for recursive paper discovery
-            ids.append(xml_dict["arxiv_id"])
+                    # check if the entry is about the correct paper by comparing titles
+                    process_title = lambda t: t.lower().replace(" ", "")
+                    t1 = process_title(title) # bib entry
+                    t2 = process_title(entry_title) # search result entry
+                    if not t1 == t2:
+                        # print(f"t1: {t1}")
+                        # print(f"t2: {t2}")
+                        continue
 
-    # incorporate additional ids (if the corresponding doc is found in the bib file)
-    for additional_id in additional_citation_ids:
+                    # record useful information in the citation record aka xml_dict
+                    for entry_item in entry:
+                        for label in ["<id>", "<title>", "<summary>", "<name>"]:
+                            if entry_item.startswith(label):
+                                clean_label = label.lstrip("<").rstrip(">")
+                                item_body = entry_item.lstrip(label).rstrip(label.replace("<", "</"))
+                                if clean_label == "name":
+                                    try: # check if label already exists (for multiple author names)
+                                        xml_dict[clean_label] += f" {item_body}"
+                                    except KeyError:
+                                        xml_dict[clean_label] = item_body
+                                elif clean_label == "id":
+                                    xml_dict["arxiv_id"] = item_body.split("/")[-1]
+                                else:
+                                    xml_dict[clean_label] = item_body
+
+                # check if the paper was found via arxiv search
+                if xml_dict == {"bib_id": f"{bib_id}"}:
+                    # TODO: maybe get the latex paper from somewhere else?
+                    continue
+
+                # store this citation (xml_dict) in citation_data
+                citations_data[bib_id] = xml_dict
+
+                # add id for recursive paper discovery
+                ids.append(xml_dict["arxiv_id"])
+
+    # incorporate additional records
+    if additional_citation_records is not None:
+        for additional_record in additional_citation_records.items():
+            citations_data[additional_record[0]] = additional_record[1]
+            ids.append(additional_record[1]["arxiv_id"])
+
+    # save citations_data to disk
+    with open(data_path, 'w', encoding='utf-8') as f:
+        json.dump(citations_data, f, ensure_ascii=False, indent=4)
 
     return citations_data, ids
 
@@ -179,6 +210,8 @@ def locate_main_tex_file(id):
 
 def extract_input_latex(path):
     lines = []
+    if not path.endswith(".tex"):
+        path += ".tex"
     with open(path, "r") as tex:
         for line in tex.readlines():
             if not line.startswith("\n"): # removes linebreaks
@@ -201,7 +234,7 @@ def extract_full_latex_textbody(id):
             if collect_lines and not line.startswith("\n"): # removes linebreaks
 
                 if line.startswith("\\input"): # handle latex \input
-                    input_file = line.lstrip("\\input{").split("}")[0]
+                    input_file = line.split("{")[1].split("}")[0]
                     input_path = f"data/{id}/{input_file}"
                     input_lines = extract_input_latex(input_path)
                     for input_line in input_lines:
@@ -231,7 +264,8 @@ def identify_citing_sentences(source_doc, bib_id):
     
     return citing_sents
     
-def get_target_sections(target_doc, section_labels=["\\section", "\\subsection"])
+def get_target_sections(target_doc, section_labels=["\\section", "\\subsection"]):
+    pass
 
 
 def get_source_citations(source_id, target_citation_record):
@@ -239,7 +273,13 @@ def get_source_citations(source_id, target_citation_record):
     target_arxiv_id = target_citation_record["arxiv_id"]
 
     source_doc, _ = extract_full_latex_textbody(source_id)
-    target_doc, _ = extract_full_latex_textbody(target_arxiv_id)
+    target_doc = None
+    try:
+        target_doc, _ = extract_full_latex_textbody(target_arxiv_id)
+    except AssertionError: # download if target not found
+        dl_arxiv(target_arxiv_id)
+        target_doc, _ = extract_full_latex_textbody(target_arxiv_id)
+    assert target_doc != None
 
     citing_sents = identify_citing_sentences(source_doc, target_bib_id)
 
@@ -259,7 +299,7 @@ if __name__ == "__main__":
     # for id in ["1607.06450"]:# , "1409.0473", "1703.03906"]:
     #     # dl_arxiv(id)
     #     path = get_bbl_path_from_arxiv_id(id)
-    #     citations_data, ids = get_citation_data_from_bbl(path)
+    #     citations_data, ids = get_citations_data_from_bbl(path)
     #     # print(citations_data)
 
     #     # recursion_depth = 3
@@ -273,7 +313,7 @@ if __name__ == "__main__":
     #     #             continue # skip if extraction failed
 
     #     #         _path = get_bbl_path_from_arxiv_id(x)
-    #     #         _, _new_ids = get_citation_data_from_bbl(_path)
+    #     #         _, _new_ids = get_citations_data_from_bbl(_path)
     #     #         [new_ids.append(y) for y in _new_ids]
 
     #     #     ids_to_process = new_ids
@@ -281,11 +321,24 @@ if __name__ == "__main__":
     #     #     recursion_depth -= 1
 
 
-    id = "1607.06450"
+    id = "2108.09084" # Fastformer
     path = get_bbl_path_from_arxiv_id(id)
-    citations_data, _ = get_citation_data_from_bbl(path)
-    target_bib_id = list(citations_data.keys())[0]
+    citations_data, _ = get_citations_data_from_bbl(path, additional_citation_records={
+        "vaswani2017attention": {
+            "bib_id": "vaswani2017attention",
+            "arxiv_id": "1706.03762", 
+            "title": "Attention is all you need", 
+            "summary": "The dominant sequence transduction models are based on complex recurrent or convolutional neural networks in an encoder-decoder configuration. The best performing models also connect the encoder and decoder through an attention mechanism. We propose a new simple network architecture, the Transformer, based solely on attention mechanisms, dispensing with recurrence and convolutions entirely. Experiments on two machine translation tasks show these models to be superior in quality while being more parallelizable and requiring significantly less time to train. Our model achieves 28.4 BLEU on the WMT 2014 English-to-German translation task, improving over the existing best results, including ensembles by over 2 BLEU. On the WMT 2014 English-to-French translation task, our model establishes a new single-model state-of-the-art BLEU score of 41.8 after training for 3.5 days on eight GPUs, a small fraction of the training costs of the best models from the literature. We show that the Transformer generalizes well to other tasks by applying it successfully to English constituency parsing both with large and limited training data.", 
+            "name": "Ashish Vaswani Noam Shazeer Niki Parmar Jakob Uszkoreit Llion Jones Aidan~N Gomez Lukasz Kaiser Illia Polosukhin"
+        }
+    })
+    print(len(citations_data.items()))
+
+    target_bib_id = "vaswani2017attention" # Transformer
     target_citation_record = citations_data[target_bib_id]
+
+    # DEBUG
+    exit
 
     citing_sents, target_doc_sections = get_source_citations(id, target_citation_record)
 
@@ -330,6 +383,7 @@ if __name__ == "__main__":
         json.dump({
             f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}": {
                 "query_doc_id": f"{id}",
+                "target_doc_id": f"{target_citation_record["arxiv_id"]}",
                 "records": similarity_records
             }
         }, f, ensure_ascii=False, indent=4)
